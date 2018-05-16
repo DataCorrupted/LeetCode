871d75fbcce69e4b91e4897e8076c5e8865b1c25
Peter Rong <PeterRong96@gmail.com>
2018-05-16 23:10:36 +0800

    39 passed
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ src/39.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    map<int, vector<vector<int> > > combo_map;
+    const vector<vector<int> > recursiveCombSum(const vector<int>& candidates, const int target){
+        if (combo_map[target].size() != 0){
+            return combo_map[target];
+        }
+        for (int i=0; i<candidates.size(); i++){
+            if (target - candidates[i] >= 0){
+                auto temp = recursiveCombSum(candidates, target-candidates[i]);
+                for (int j=0; j<temp.size(); j++){
+                    auto vec = temp[j];
+                    if (vec.size() == 0 || candidates[i] <= vec[0]){
+                        vec.insert(vec.begin(), candidates[i]);
+                        combo_map[target].push_back(vec);
+                    }
+                }
+            }
+        }
+        return combo_map[target];
+    }
+        
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        combo_map[0] = vector<vector<int> >();
+        combo_map[0].push_back(vector<int>());
+        sort(candidates.begin(), candidates.end());
+        return recursiveCombSum(candidates, target);
+    }
+};
+
+int main(){
+	int arr[] = {5,10,8,4,3,12,9};
+	vector<int> v = vector<int>(arr, arr + 7);
+	Solution s;
+	auto vec = s.combinationSum(v, 27);
+	for (int i=0; i<vec.size(); i++){
+		for (int j=0; j<vec[i].size(); j++){
+			cout << vec[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
