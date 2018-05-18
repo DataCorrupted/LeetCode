a65aeb1dcf83422e3224d32e49fa17a0cdcb9295
Peter Rong <PeterRong96@gmail.com>
2018-05-18 15:42:13 +0800

    27 passed
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ 27.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int curr = 0;
+        for (int i=0; i<nums.size(); i++){
+            if (nums[i] != val){
+                swap(nums[curr], nums[i]);
+                curr ++;
+            }            
+        }
+        nums.erase(nums.begin() + curr, nums.end());
+        return curr;
+    }
+};
