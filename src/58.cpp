5ba1ccb57cfdc64b7ddcb612cd809a092c8b5ba8
Peter Rong <PeterRong96@gmail.com>
2018-05-21 17:50:01 +0800

    58 passed
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ src/58.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        s = s + ' ';
+        int last_len  = 0;
+        int counter = 0;
+        int i = 0;
+        while (i < s.size()) {
+            if (s[i] == ' '){
+                last_len = counter;
+                counter = 0;
+                while (i < s.size() && s[i] == ' ' ){ i++; }
+            } else {
+                counter ++;
+                i++;
+            }
+        }
+        return last_len;
+    }
+};
--- src/debug.cpp
+++ src/debug.cpp
@@ -8,14 +8,15 @@
 
 using namespace std;
 
-#include "78.cpp"
+#include "58.cpp"
 #include "printUtil.cpp"
 
 int main(){
 	vector<int> v({1, 2, 3});
+	string s = "a ";
 
 	Solution solver;
-	auto result = solver.subsets(v);
+	auto result = solver.lengthOfLastWord(s);
 	cout << result;
 }
 
