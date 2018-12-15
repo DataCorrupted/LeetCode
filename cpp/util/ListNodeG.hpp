ca6f79bf976b581ff81e75c6bd2648b94bcd78fe
DataCorrupted <PeterRong96@gmail.com>
2018-12-15 21:44:56 +0800

    added a method to listnode
    Signed-off-by: DataCorrupted <PeterRong96@gmail.com>

--- cpp/util/ListNodeG.hpp
+++ cpp/util/ListNodeG.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 template <class T>
 struct ListNodeG {
@@ -9,6 +10,16 @@ struct ListNodeG {
 		if (next) { delete next; }
 	}
 
+	static ListNodeG<T>* vectorToListNode(std::vector<T> valVec){
+		std::vector<ListNodeG<int>*> nodeVec;
+		for (T x: valVec){
+			nodeVec.push_back(new ListNodeG(x));
+		}
+		for (unsigned i = 1; i < valVec.size(); i++){
+			nodeVec[i-1]->next = nodeVec[i];
+		}
+		return (nodeVec.size() == 0) ? NULL: nodeVec[0];
+	}
 	// This function cannot be overloaded because both operand is pointer.
 	static bool listsEq(const ListNodeG* lhs, const ListNodeG* rhs){
 		if (!lhs && !rhs) { return true; }
