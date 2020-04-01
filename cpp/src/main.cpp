944e32b21751cb9689796316e4632f78b4f9faed
Peter Rong <PeterRong96@gmail.com>
2020-04-01 15:25:32 -0700

    changed file structure and test framework
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ cpp/src/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define DEBUG
+
+using namespace std;
+
+#include "DebugUtil.h"
+#include "PrintUtil.h"
+#include "solutions/659.cpp"
+
+int main() {
+  for (auto pair: TEST_NAME_FUNC_PAIR_VEC) {
+    std::string& name = pair.first;
+    bool test_result = pair.second();
+    cout << "test " << name << " passed ... " << 
+        ((test_result) ? GREEN : RED) << boolalpha
+         << test_result << WHITE << endl;
+  }
+  return 0;
+}
---
+++ cpp/src/util/DebugUtil.h
@@ -0,0 +1,23 @@
+#ifndef DEBUG_UTIL
+#define DEBUG_UTIL
+
+#include <string>
+#include <vector>
+
+static std::string RED = "\033[1;31m";
+static std::string GREEN = "\033[1;32m";
+static std::string WHITE = "\033[0m";
+
+typedef std::pair<std::string, bool (*)()> TestNameFuncPair;
+typedef std::vector<TestNameFuncPair> TestNameFuncPairVec;
+static TestNameFuncPairVec TEST_NAME_FUNC_PAIR_VEC = TestNameFuncPairVec();
+#define REGISTER_TEST(x)               \
+  static bool test##x();               \
+  struct SolutionTest##x##Register {   \
+    SolutionTest##x##Register() {      \
+      TEST_NAME_FUNC_PAIR_VEC.push_back(TestNameFuncPair(#x, &test##x)); \
+    }                                  \
+  } solutionTest##x##Register;         \
+  static bool test##x()
+
+#endif
